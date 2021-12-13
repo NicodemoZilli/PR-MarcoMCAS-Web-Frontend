3684eedb4fe9dcda529c6c75c8462c4fb7a6213b
Nicodemo Zilli <nicozv13@gmail.com>
2021-12-12 20:41:29 -0600

    creacion total del template

---
+++ independencia.php
@@ -0,0 +1,32 @@
+<?php
+  include_once 'html/head.html';
+?>
+<header>
+      <div class="contenedor">
+        <a href="index.php" class='logo'><img src="media/lcenidet.png" alt=""></a>
+        <input type="checkbox" id="menu-bar">
+        <label class="icon-menu" for="menu-bar"></label>
+        <nav class="menu">
+          <a href="index.php" >Inicio</a>
+          <a href="genericidad.php">Gen</a>
+          <a href="flexibilidad.php">Flex</a>
+          <a href="legibilidad.php">Leg</a>
+          <a href="proedo.php">PE</a>
+          <a href="procomp.php">PC</a>
+          <a href="independencia.php" class="active">Ind</a>
+          <a href="uresponsabilidad.php">UR</a>
+          <a href="aislamiento.php">Ais</a>
+        </nav>
+      </div>
+</header>
+
+<div class="content">
+
+  <h1 style=" width: 100vw; margin-top: 30vh; color: #fff; text-align: center;">Una disculpa, esta p&aacute;gina a&uacute;n se encuentra en construcci&oacute;n</h1>
+
+</div>
+
+  <?php
+    //include_once 'html/console.html';
+    include_once 'html/footer.html';
+  ?>
